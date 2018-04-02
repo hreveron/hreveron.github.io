1315473fe12587e9af5a034fdfebdf4e3c3de1b1
Hector Reveron <hreveron@gmail.com>
2018-04-02 18:11:26 -0400

    Update coinet.js
    transferir JCL a una cartera

--- coinet.js
+++ coinet.js
@@ -112,7 +112,7 @@ function getBalance(accountId) {
 
 
 
-function transferir(sourceSecretKey, receiverPublicKey, amount){
+function transferir(assetName, issuingPublicKey, sourceSecretKey, receiverPublicKey, amount){
     // The source account is the account we will be signing and sending from.
     //var sourceSecretKey = 'SAIRWUXWUSD46JSMVNVPHZZB6WHGJEARIXBRNIIW6HZQ37T6IGIFFOWL';
 
@@ -128,6 +128,9 @@ function transferir(sourceSecretKey, receiverPublicKey, amount){
     // StellarSdk.Network.usePublicNetwork();
     StellarSdk.Network.useTestNetwork();
 
+    var tokenObj = new StellarSdk.Asset(assetName, issuingPublicKey);
+
+
     // Transactions require a valid sequence number that is specific to this account.
     // We can fetch the current sequence number for the source account from Horizon.
     server.loadAccount(sourcePublicKey)
@@ -137,7 +140,11 @@ function transferir(sourceSecretKey, receiverPublicKey, amount){
           .addOperation(StellarSdk.Operation.payment({
             destination: receiverPublicKey,
             // The term native asset refers to lumens
-            asset: StellarSdk.Asset.native(),
+            //asset: StellarSdk.Asset.native(),
+            asset: tokenObj,
+
+
+
             // Specify 350.1234567 lumens. Lumens are divisible to seven digits past
             // the decimal. They are represented in JS Stellar SDK in string format
             // to avoid errors from the use of the JavaScript Number data structure.
@@ -178,6 +185,54 @@ function transferir(sourceSecretKey, receiverPublicKey, amount){
 
 
 
+function createAsset(assetName, issuingAcountSecret, receivingAccountSecret, tokenAmount){
+    StellarSdk.Network.useTestNetwork();
+    //var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+    var server = new StellarSdk.Server(horizontUrl, {allowHttp: true});
+
+    // Keys for accounts to issue and receive the new asset
+    var issuingKeys = StellarSdk.Keypair
+      .fromSecret(issuingAcountSecret);
+    var receivingKeys = StellarSdk.Keypair
+      .fromSecret(receivingAccountSecret);
+
+    // Create an object to represent the new asset
+    var tokenObj = new StellarSdk.Asset(assetName, issuingKeys.publicKey());
+
+    // First, the receiving account must trust the asset
+    server.loadAccount(receivingKeys.publicKey())
+      .then(function(receiver) {
+        var transaction = new StellarSdk.TransactionBuilder(receiver)
+          // The `changeTrust` operation creates (or alters) a trustline
+          // The `limit` parameter below is optional
+          .addOperation(StellarSdk.Operation.changeTrust({
+            asset: tokenObj,
+            limit: tokenAmount
+          }))
+          .build();
+        transaction.sign(receivingKeys);
+        return server.submitTransaction(transaction);
+      })
+
+      // Second, the issuing account actually sends a payment using the asset
+      .then(function() {
+        return server.loadAccount(issuingKeys.publicKey())
+      })
+      .then(function(issuer) {
+        var transaction = new StellarSdk.TransactionBuilder(issuer)
+          .addOperation(StellarSdk.Operation.payment({
+            destination: receivingKeys.publicKey(),
+            asset: tokenObj,
+            amount: '10'
+          }))
+          .build();
+        transaction.sign(issuingKeys);
+        return server.submitTransaction(transaction);
+      })
+      .catch(function(error) {
+        console.error('Error!', error);
+      });    
+}
 
 
 
@@ -186,7 +241,26 @@ function transferir(sourceSecretKey, receiverPublicKey, amount){
 
 
 
+createAccount();
 
+const assetName = 'JCL';
+const issuingAcountSecret = 'SCFGVU324V2MRXCS42ZL2LMOY6OQ3PSLS5LDHIR4SXXJ52CMHMCIAYTH';
+const receivingAccountSecret = 'SAMV4HFMD6R4NPUQLKLQBJLYVU2IPEWS6JKMHH7PSZCIUY5QOKJ5NKO5';
+const tokenInitialAmount = '1000000';
+const amount = '10';
+const receiverPublicKey = 'GBWOJS22DWLJWPQ6BQ6PGAQVLMQKY3TBQMQYYNIH4C4XO4IPJKNDR6VM';
+const issuingPublicKey = 'GBHYXZDEQFEPCCZAXOY5JCP5N2EP6R2Y7ZJWEPBIUYYVHZNA77O23RJ2';
+
+
+//createAsset(assetName, issuingAcountSecret, receivingAccountSecret, tokenAmount);
+
+//getBalance('GBWOJS22DWLJWPQ6BQ6PGAQVLMQKY3TBQMQYYNIH4C4XO4IPJKNDR6VM');
+//getBalance('GBHYXZDEQFEPCCZAXOY5JCP5N2EP6R2Y7ZJWEPBIUYYVHZNA77O23RJ2');
+//getBalance('GBWOJS22DWLJWPQ6BQ6PGAQVLMQKY3TBQMQYYNIH4C4XO4IPJKNDR6VM');
+
+//transferir(assetName, issuingPublicKey, issuingAcountSecret, receiverPublicKey, amount);
+transferir(assetName, issuingPublicKey, issuingAcountSecret, receiverPublicKey, amount);
+//getBalance('GBBEVVRCEY6N3KSRAQSB47XUL27J2NPGPQKA6DA5LJD3HU74R33WW6VT');
 
 //createAccount();
 //getBalance('GAG4LX4WI5BQN74SZ656KDEUKR7VCMOAW3DU32BHF3JX4MX5BLP2RAJM');
@@ -195,6 +269,12 @@ function transferir(sourceSecretKey, receiverPublicKey, amount){
 
 
 /*
+publicKey: GDZN5Z3OGQOI526H2AL6IFJKVA7FLFFFSVML7JH7TQT252F5BRS6BUID
+secret: SADSTHI53AMJFKQ5LCQWPRV4KT52X26GEGPAKNYESTZPBURVZ34S23ES
+
+
+
+
 GAG4LX4WI5BQN74SZ656KDEUKR7VCMOAW3DU32BHF3JX4MX5BLP2RAJM
 
 publicKey: GBBEVVRCEY6N3KSRAQSB47XUL27J2NPGPQKA6DA5LJD3HU74R33WW6VT
@@ -204,7 +284,9 @@ publicKey: GCCKXMUD3ESPZBTTNLKNOSOPCJHCEWX6KFTACAQ5QAOQC5IL45SDKZYM
 secret: SDEFRX3AFXJD2NIOV3YPNAF4VJORZLSSQKDFMXTMMPYZSTZJ5V67RAMW
 */
 
+/*
 sourceSecretKey = 'SCN7QPTICHQXP6AWZDZYPWL5NY5W26G54JXJ65VVTDSLQUT6SMMPJTHJ';
 receiverPublicKey = 'GCCKXMUD3ESPZBTTNLKNOSOPCJHCEWX6KFTACAQ5QAOQC5IL45SDKZYM';
 amount = '350.1234567';
 transferir(sourceSecretKey, receiverPublicKey, amount);
+*/
